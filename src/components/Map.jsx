5ab4cfadba66681adddc43c7401ee38e5c155e44
Adil Naseem <adilnaseemsheikh@gmail.com>
2023-09-15 21:04:02 +0500

    router completed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      Position: {lat}, {lng}
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
